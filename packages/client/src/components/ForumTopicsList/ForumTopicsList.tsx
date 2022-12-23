46567d63d867586b17caa0670edd3f8822d4c6e7
krasotun <servrostov@gmail.com>
2022-12-23 23:12:26 +0300

    ref change render topics

--- packages/client/src/components/ForumTopicsList/ForumTopicsList.tsx
+++ packages/client/src/components/ForumTopicsList/ForumTopicsList.tsx
@@ -1,11 +1,34 @@
 import ForumTopic from '../ForumTopic/ForumTopic'
+import { IForumTopicProps } from '../ForumTopic/ForumTopicProps'
 import './ForumTopicsList.scss'
+const topics: IForumTopicProps[] = [
+  {
+    topicAuthor: 'Петр',
+    topicTitle: 'Тема от Петра',
+  },
+  {
+    topicAuthor: 'Антон',
+    topicTitle: 'Тема от Антона',
+  },
+  {
+    topicAuthor: 'Стас',
+    topicTitle: 'Тема от Стаса',
+  },
+  {
+    topicAuthor: 'Настя',
+    topicTitle: 'Тема от Насти',
+  },
+]
+
 function ForumTopicsList() {
   return (
     <ul className="topic-list d-flex flex-column">
-      <ForumTopic topicAuthor="Петр" topicTitle="Тема от Петра" />
-      <ForumTopic topicAuthor="Антон" topicTitle="Тема от Антона" />
-      <ForumTopic topicAuthor="Стас" topicTitle="Tema от Стаса" />
+      {topics.map(topic => (
+        <ForumTopic
+          topicAuthor={topic.topicAuthor}
+          topicTitle={topic.topicTitle}
+        />
+      ))}
     </ul>
   )
 }
