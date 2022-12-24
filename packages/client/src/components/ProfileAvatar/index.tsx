33c57f5356449139cc300fa2ba00554d71af48d7
gineff <canone@inbox.ru>
2022-12-25 00:10:15 +0300

    ft: add update avatar features

--- packages/client/src/components/ProfileAvatar/index.tsx
+++ packages/client/src/components/ProfileAvatar/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+import { updateAvatar } from '../../services/userController';
 import Avatar from '../../components/Avatar';
+import UploadFile from '../../components/UploadFile';
 import './index.css';
 
 type AvatarType = string;
@@ -9,13 +12,35 @@ type profileAvataProps = {
 const yaResources = 'https://ya-praktikum.tech/api/v2/resources';
 
 export default ({ avatar }: profileAvataProps) => {
-  const image = avatar ? `${yaResources}${avatar}` : "'/src/assets/king.png'";
+  //ToDo убрать(изменить) после того, как будет сделана api
+  const initialImage = avatar ? `${yaResources}${avatar}` : "'/src/assets/king.png'";
+  const [image, setImage] = useState(initialImage);
+  // end ToDo
 
-  const onAvatarClick = async () => {};
+  const [modalOptions, setModalOptions] = useState({});
+
+  const onAvatarClick = async () => {
+    const promise = new Promise(res => {
+      setModalOptions({ res });
+    });
+
+    const file = await promise;
+    setModalOptions({});
+
+    if (file) {
+      //ToDo убрать(изменить) после того как будет api () = ormData.append('avatar', file as Blob);
+      const formData = new FormData();
+      formData.append('resource', file as Blob);
+      const xhr = await updateAvatar(formData);
+      const response = (xhr as XMLHttpRequest).response;
+      setImage(`${yaResources}/${response.path}`);
+    }
+  };
 
   return (
     <div className="user-profile__avatar-wrapper" onClick={onAvatarClick}>
       <Avatar className="user-profile__avatar" image={image} />
+      {Object.keys(modalOptions).length !== 0 ? <UploadFile options={modalOptions} /> : null}
     </div>
   );
 };
--- packages/client/src/services/userController.ts
+++ packages/client/src/services/userController.ts
@@ -4,8 +4,29 @@ export async function updateProfile(data: any) {
   await userApi.updateProfile(data);
 }
 
-export async function updateAvatar(data: any) {
-  await userApi.updateAvatar(data);
+export async function updateAvatar(data: FormData) {
+  // ToDo заменить когда будет готово апи
+  const yaResources = 'https://ya-praktikum.tech/api/v2/resources';
+
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', yaResources);
+
+    xhr.onload = () => {
+      if (xhr.status >= 300) {
+        reject(xhr);
+      } else {
+        resolve(xhr);
+      }
+    };
+    xhr.onabort = reject;
+    xhr.onerror = reject;
+
+    xhr.withCredentials = true;
+    xhr.responseType = 'json';
+    xhr.send(data);
+  });
 }
 
 export async function updatePassword(data: any) {
