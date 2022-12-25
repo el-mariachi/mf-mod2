4de7568377f955ead5e6a8a1f44d45593cfbbc22
gineff <canone@inbox.ru>
2022-12-25 21:08:14 +0300

    fx: fx eslint errors

--- packages/client/src/api/userApi.ts
+++ packages/client/src/api/userApi.ts
@@ -1,6 +1,22 @@
+//import { httpXML, httpFetch } from "../utils/http";
 
-export default {
-  updateProfile: (data: any)=> {},
-  updateAvatar: (data: any)=> {},
-  updatePassword: (data: any)=> {},
-};
+const API_URL = "https://ya-praktikum.tech/api/v2";
+
+type ProfileData = Omit<User, "id" | "avatar">;
+
+const updateProfile = async (data: ProfileData) => {
+  ""
+  //return httpXML.post(`${API_URL}/user/profile`);
+}
+
+const updateAvatar = async (data: any) => {
+  ""
+  //return httpXML.post(`${API_URL}/user/profile/avatar`);
+
+}
+const updatePassword = async (data: any) => {
+  ""
+  //return httpXML.post(`${API_URL}/user/password`);
+}
+
+export default {updateProfile, updateAvatar, updatePassword}
