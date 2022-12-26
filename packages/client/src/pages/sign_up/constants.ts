fc0029097849a73e4965e2d659a0146e8e05fac6
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-12-26 16:13:46 +0300

    ft: Add validation, compare passwords

--- packages/client/src/pages/sign_up/constants.ts
+++ packages/client/src/pages/sign_up/constants.ts
@@ -1,4 +1,26 @@
-const inputData = [
+const defaultValues = {
+  first_name: '',
+  second_name: '',
+  login: '',
+  email: '',
+  phone: '',
+  password: '',
+  password2: '',
+}
+
+export type FieldName = keyof typeof defaultValues
+
+export type InputData = {
+  name: FieldName
+  label: string
+  message: string
+  type: string
+  placeholder: string
+  test: RegExp
+  valid: boolean
+}
+
+const inputData: InputData[] = [
   {
     name: 'first_name',
     label: 'Имя',
@@ -6,6 +28,7 @@ const inputData = [
     type: 'text',
     placeholder: 'Имя',
     test: /^([A-Z][A-Za-z-]*$)|([А-Я][А-Яа-я-]*$)/,
+    valid: true,
   },
   {
     name: 'second_name',
@@ -14,6 +37,7 @@ const inputData = [
     type: 'text',
     placeholder: 'Фамилия',
     test: /^([A-Z][A-Za-z]*$)|([А-Я][А-Яа-я-]*$)/,
+    valid: true,
   },
   {
     name: 'login',
@@ -22,6 +46,7 @@ const inputData = [
     type: 'text',
     placeholder: 'Логин',
     test: /^(?=.*[a-zA-Z])[a-zA-Z0-9_-]{3,20}$/,
+    valid: true,
   },
   {
     name: 'email',
@@ -30,6 +55,7 @@ const inputData = [
     type: 'email',
     placeholder: 'Email',
     test: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+    valid: true,
   },
   {
     name: 'phone',
@@ -38,6 +64,7 @@ const inputData = [
     type: 'text',
     placeholder: 'Телефон',
     test: /^\+?\d{10,15}$/i,
+    valid: true,
   },
   {
     name: 'password',
@@ -46,15 +73,17 @@ const inputData = [
     type: 'password',
     placeholder: 'Пароль',
     test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+    valid: true,
   },
   {
     name: 'password2',
     label: 'Повторите пароль',
-    message: 'Пароли не совпадают',
+    message: 'Пароль не соответствует требованиям',
     type: 'password',
     placeholder: 'Повторите пароль',
     test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+    valid: true,
   },
 ]
 
-export { inputData }
+export { inputData, defaultValues }
