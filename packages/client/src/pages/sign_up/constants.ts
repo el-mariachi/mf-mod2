cf427cadac86bdcd205861ebdd81bc2ac9aa60e4
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-12-26 13:50:42 +0300

    ft: Build signup form

---
+++ packages/client/src/pages/sign_up/constants.ts
@@ -0,0 +1,60 @@
+const inputData = [
+  {
+    name: 'first_name',
+    label: 'Имя',
+    message: 'Поле заполнено некорректно',
+    type: 'text',
+    placeholder: 'Имя',
+    test: /^([A-Z][A-Za-z-]*$)|([А-Я][А-Яа-я-]*$)/,
+  },
+  {
+    name: 'second_name',
+    label: 'Фамилия',
+    message: 'Поле заполнено некорректно',
+    type: 'text',
+    placeholder: 'Фамилия',
+    test: /^([A-Z][A-Za-z]*$)|([А-Я][А-Яа-я-]*$)/,
+  },
+  {
+    name: 'login',
+    label: 'Логин',
+    message: 'Логин не соответствует требованиям',
+    type: 'text',
+    placeholder: 'Логин',
+    test: /^(?=.*[a-zA-Z])[a-zA-Z0-9_-]{3,20}$/,
+  },
+  {
+    name: 'email',
+    label: 'Email',
+    message: 'Некорректный email',
+    type: 'email',
+    placeholder: 'Email',
+    test: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+  },
+  {
+    name: 'phone',
+    label: 'Телефон',
+    message: 'Некорректный телефон',
+    type: 'text',
+    placeholder: 'Телефон',
+    test: /^\+?\d{10,15}$/i,
+  },
+  {
+    name: 'password',
+    label: 'Пароль',
+    message: 'Пароль не соответствует требованиям',
+    type: 'password',
+    placeholder: 'Пароль',
+    test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+  },
+  {
+    name: 'password2',
+    label: 'Повторите пароль',
+    message: 'Пароли не совпадают',
+    type: 'password',
+    placeholder: 'Повторите пароль',
+    test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+  },
+]
+
+export { inputData }
--- packages/client/src/pages/sign_up/sign_up.tsx
+++ packages/client/src/pages/sign_up/sign_up.tsx
@@ -3,19 +3,83 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { useForm } from 'react-hook-form'
+import '../../../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './sign_up.scss'
+import { inputData } from './constants'
+import { MouseEvent, useState } from 'react'
+// import {} from 'react-router-dom'
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState(inputData)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: 'onTouched',
+    reValidateMode: 'onSubmit',
+  })
+
+  const formSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  const goToLogin = (e: MouseEvent) => {
+    e.preventDefault()
+    console.log('nav')
+  }
+
   return (
     <div className="sign_up w-100 h-100 d-flex position-fixed align-items-center justify-content-center">
-      <Container fluid className="sign_up bg-secondary d-flex">
-        <Container className="sm">
-          <Row className="justify-content-sm-center">
-            <Col sm="auto">
-              <Form>Превед</Form>
-            </Col>
-          </Row>
-        </Container>
+      <Container className="sign_up-form mx-auto">
+        <div className="bg-light rounded-4 p-5">
+          <Form onSubmit={handleSubmit(formSubmit)}>
+            <Row className="mb-4">
+              <Col sm={{ span: 9, offset: 3 }}>
+                <h1 className="h3">Регистрация</h1>
+              </Col>
+            </Row>
+
+            {inputs.map((input, index) => (
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId={`signUp-${index}`}>
+                <Form.Label column sm="3">
+                  {input.label}:
+                </Form.Label>
+                <Col sm={9}>
+                  <Form.Control
+                    type={input.type}
+                    isInvalid={errors[input.name] !== undefined}
+                    {...register(input.name, {
+                      required: 'Поле должно быть заполнено',
+                      pattern: {
+                        value: input.test,
+                        message: input.message,
+                      },
+                    })}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {String(errors[input.name]?.message)}
+                  </Form.Control.Feedback>
+                </Col>
+              </Form.Group>
+            ))}
+
+            <Form.Group as={Row}>
+              <Col sm={{ span: 9, offset: 3 }}>
+                <Button type="submit">Создать аккаунт</Button>
+                <Button as="a" variant="link" onClick={goToLogin}>
+                  Вход
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </div>
       </Container>
     </div>
   )
