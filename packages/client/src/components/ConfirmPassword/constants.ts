0ee9b17684c1efaaf1d205deaaced3d6128db062
Dmitri Sokolov <mitson.mbox@gmail.com>
2023-01-04 19:36:15 +0300

    ft: Validate ConfirmPassword

--- packages/client/src/components/ConfirmPassword/constants.ts
+++ packages/client/src/components/ConfirmPassword/constants.ts
@@ -17,4 +17,36 @@ export const PASSWORD_FIELDS: ProfileInputProps[] = [
     value: '',
     label: 'Повторите новый пароль',
   },
-];
+]
+
+export const changePasswordInputData = [
+  {
+    name: 'newPassword',
+    label: 'Новый пароль',
+    message: 'Пароль не соответствует требованиям',
+    type: 'password',
+    placeholder: 'Новый пароль',
+    test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+  },
+  {
+    name: 'confirmPassword',
+    label: 'Повторите пароль',
+    message: 'Пароль не соответствует требованиям',
+    type: 'password',
+    placeholder: 'Повторите пароль',
+    test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+  },
+  {
+    name: 'oldPassword',
+    label: 'Старый пароль',
+    message: 'Пароль не соответствует требованиям',
+    type: 'password',
+    placeholder: 'Старый пароль',
+    test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+  },
+] as const
+
+export type ChangePasswordFormStruct = Record<
+  typeof changePasswordInputData[number]['name'],
+  string
+>
