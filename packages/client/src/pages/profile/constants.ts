03e2dac748b04967ad14795dfdb4f0b06ae707fe
Dmitri Sokolov <mitson.mbox@gmail.com>
2023-01-04 16:45:07 +0300

    ft: Validate Profile form

---
+++ packages/client/src/pages/profile/constants.ts
@@ -0,0 +1,107 @@
+const profileDefaultValues = {
+  first_name: '',
+  second_name: '',
+  display_name: '',
+  login: '',
+  email: '',
+  phone: '',
+  password: '',
+}
+
+const passwordDefaultValues = {
+  password: '',
+  confirmPassword: '',
+}
+
+type FieldNameType = keyof typeof profileDefaultValues
+
+type InputProps = {
+  name: FieldNameType
+  label: string
+  message: string
+  type: string
+  placeholder: string
+  test: RegExp
+}
+
+const profileFormInputs: InputProps[] = [
+  {
+    name: 'first_name',
+    label: 'Имя',
+    message: 'Поле заполнено некорректно',
+    type: 'text',
+    placeholder: 'Имя',
+    test: /^([A-Z][A-Za-z-]*$)|([А-Я][А-Яа-я-]*$)/,
+  },
+  {
+    name: 'second_name',
+    label: 'Фамилия',
+    message: 'Поле заполнено некорректно',
+    type: 'text',
+    placeholder: 'Фамилия',
+    test: /^([A-Z][A-Za-z]*$)|([А-Я][А-Яа-я-]*$)/,
+  },
+  {
+    name: 'display_name',
+    label: 'Ник',
+    message: 'Поле заполнено некорректно',
+    type: 'text',
+    placeholder: 'Ник',
+    test: /^([A-Za-z]+$)|([А-Яа-я-]+$)/,
+  },
+  {
+    name: 'login',
+    label: 'Логин',
+    message: 'Логин не соответствует требованиям',
+    type: 'text',
+    placeholder: 'Логин',
+    test: /^(?=.*[a-zA-Z])[a-zA-Z0-9_-]{3,20}$/,
+  },
+  {
+    name: 'email',
+    label: 'Email',
+    message: 'Некорректный email',
+    type: 'email',
+    placeholder: 'Email',
+    test: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+  },
+  {
+    name: 'phone',
+    label: 'Телефон',
+    message: 'Некорректный телефон',
+    type: 'text',
+    placeholder: 'Телефон',
+    test: /^\+?\d{10,15}$/i,
+  },
+  {
+    name: 'password',
+    label: 'Пароль',
+    message: 'Пароль не соответствует требованиям',
+    type: 'password',
+    placeholder: 'Пароль',
+    test: /^(?=.*\d)(?=.*[A-ZА-Я]).{8,40}$/,
+  },
+]
+
+const DEFAULT_USER = {
+  id: 0,
+  email: 'mail@pochta.ru',
+  login: 'IvanIvanov',
+  secondName: 'Ivanov',
+  firstName: 'Ivan',
+  displayName: 'Ivan Ivanov',
+  phone: '+7 (909) 967 30 30',
+  avatar: '/src/assets/king.png',
+}
+
+const EDIT_CLASS = 'user-profile__form_edit'
+const READ_CLASS = 'user-profile__form_read'
+
+export {
+  profileFormInputs,
+  profileDefaultValues,
+  passwordDefaultValues,
+  DEFAULT_USER,
+  EDIT_CLASS,
+  READ_CLASS,
+}
--- packages/client/src/pages/profile/index.tsx
+++ packages/client/src/pages/profile/index.tsx
@@ -1,28 +1,46 @@
-import { useState, createRef } from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import { AppError, formUserErrorHandler } from '../../utils/errors_handling'
 import { updatePassword, updateProfile } from '../../services/userController'
 import ProfileAvatar from '../../components/ProfileAvatar'
-import ProfileForm from '../../components/ProfileForm'
+import FormGroupView from '../../components/FormGroupView'
 import ConfirmPassword from '../../components/ConfirmPassword'
+import {
+  profileFormInputs,
+  profileDefaultValues,
+  READ_CLASS,
+  EDIT_CLASS,
+} from './constants'
 import './index.css'
+import { useForm } from 'react-hook-form'
 
 type ProfileProps = {
   user: UserDTO | undefined
 }
+type ProfileFormData = typeof profileDefaultValues
+
+enum Mode {
+  Edit,
+  View,
+}
 
 const Profile = ({ user }: ProfileProps) => {
-  const [readOnly, setReadOnly] = useState(true)
+  const [mode, setMode] = useState(Mode.View)
   const [modalOptions, setModalOptions] = useState({})
   const [submitError, setSubmitError] = useState('')
-  const refForm = createRef()
 
-  const saveChanges = async () => {
-    const form = refForm.current as HTMLFormElement
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProfileFormData>({
+    mode: 'onTouched',
+    reValidateMode: 'onChange',
+    defaultValues: profileDefaultValues,
+  })
 
-    const formData = Object.fromEntries(new FormData(form).entries())
+  const saveChanges = async (formData: ProfileFormData) => {
     const { password: newPassword } = formData
-    delete formData.password
 
     if (newPassword) {
       const promise = new Promise(res => {
@@ -36,21 +54,37 @@ const Profile = ({ user }: ProfileProps) => {
         await updatePassword(passwords)
       }
     }
-    await updateProfile(formData)
-      .catch((error : AppError) => formUserErrorHandler(error, setSubmitError))
+    await updateProfile(formData).catch((error: AppError) =>
+      formUserErrorHandler(error, setSubmitError)
+    )
   }
 
-  const handleButtonClick = async () => {
-    if (!readOnly) {
-      await saveChanges()
-    }
-    setReadOnly(!readOnly)
+  const editMode = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    setMode(Mode.Edit)
   }
+
+  const formSubmit = async (data: ProfileFormData) => {
+    await saveChanges(data)
+    setMode(Mode.View)
+  }
+
+  const formControls = profileFormInputs.map(controlProps => (
+    <FormGroupView
+      register={register}
+      errors={errors}
+      formName="userProfileForm"
+      controlProps={controlProps}
+      readOnly={Boolean(mode)}
+    />
+  ))
 
   return (
     <div className="user-profile">
       <Container fluid="sm">
-        <Form className="user-profile__form mt-5" ref={refForm as React.RefObject<HTMLFormElement>}>
+        <Form
+          className="user-profile__form mt-5"
+          onSubmit={handleSubmit(formSubmit)}>
           <Row>
             <Col sm={4} className="px-0">
               <ProfileAvatar avatar={user?.avatar} />
@@ -61,12 +95,21 @@ const Profile = ({ user }: ProfileProps) => {
               ) : (
                 ''
               )}
-
-              <ProfileForm user={user} readOnly={readOnly} />
-
-              <Button variant="dark" onClick={handleButtonClick}>
-                {readOnly ? 'Изменить данные профиля' : 'Сохранить'}
-              </Button>
+              <div
+                className={`user-profile__form ${
+                  mode ? READ_CLASS : EDIT_CLASS
+                }`}>
+                {formControls}
+              </div>
+              {mode ? (
+                <Button variant="dark" type="button" onClick={editMode}>
+                  Изменить данные профиля
+                </Button>
+              ) : (
+                <Button variant="dark" type="submit">
+                  Сохранить
+                </Button>
+              )}
             </Col>
           </Row>
         </Form>
