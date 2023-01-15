0976de15f0d2afdbfbe4194be90df8cad0e95d6d
krasotun <servrostov@gmail.com>
2023-01-15 18:38:29 +0300

    test: add test env

---
+++ packages/client/src/components/SpinnerButton/SpinnerButton.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SpinnerButton from '@components/SpinnerButton'
+import * as renderer from 'react-test-renderer'
+
+describe('SpinnerButton', () => {
+  it('renders correctly', () => {
+    // const spinnerButton = renderer.create(<SpinnerButton loading={true} />)
+    // expect(spinnerButton).toMatchSnapshot()
+  })
+})
