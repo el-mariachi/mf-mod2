83122531d61f4d81d5bec33ba4fd5e93ab73b354
krasotun <servrostov@gmail.com>
2023-01-17 12:04:19 +0300

    fix: test env

--- packages/client/src/components/SpinnerButton/NewComponent.tsx
+++
@@ -1,7 +0,0 @@
-export default function NewComponent() {
-  return (
-    <>
-      <h1>New component</h1>
-    </>
-  )
-}
--- packages/client/src/components/SpinnerButton/SpinnerButton.test.tsx
+++ packages/client/src/components/SpinnerButton/SpinnerButton.test.tsx
@@ -1,27 +1,8 @@
-// import React from 'react'
-// import * as renderer from 'react-test-renderer'
-// import { expect, it } from '@jest/globals'
 import SpinnerButton from './index'
-import NewComponent from './NewComponent'
 import { render, screen } from '@testing-library/react'
-
-// it('hello jest', () => {
-//   console.log(renderer)
-//   const spinnerButton = renderer
-//     .create(<SpinnerButton loading></SpinnerButton>)
-//     .toJSON()
-//   expect(spinnerButton).toMatchSnapshot()
-// })
-// it('hello jest', () => {
-//   const newComponent = renderer.create(<NewComponent />).toJSON()
-//   console.log(newComponent)
-// })
-
-it('render SpinnerButton', () => {
-  const renderedSpinner = render(<SpinnerButton loading />)
-  console.log(renderedSpinner)
-})
-it('render newComponent', () => {
-  const renderedNew = render(<NewComponent />)
-  console.log(renderedNew)
+describe('#SpinnerButton', () => {
+  it('should create', () => {
+    const renderedSpinner = render(<SpinnerButton loading />)
+    expect(renderedSpinner).toBeTruthy()
+  })
 })
