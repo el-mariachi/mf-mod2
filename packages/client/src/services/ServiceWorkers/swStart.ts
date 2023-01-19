ed3e3ec6922558173006e12292a87f7a9f99958d
FrankyHErus <110826856+FrankyHErus@users.noreply.github.com>
2023-01-20 01:07:11 +0300

    ft: Added service workers to project

---
+++ packages/client/src/services/ServiceWorkers/swStart.ts
@@ -0,0 +1,19 @@
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('./sw.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          )
+        })
+        .catch(error => {
+          console.log('ServiceWorker registration failed: ', error)
+        })
+    })
+  }
+}
+
+export default startServiceWorker
