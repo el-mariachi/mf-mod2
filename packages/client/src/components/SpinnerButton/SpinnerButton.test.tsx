a9a64a76bfa13939b0cf182c7a55325b25cb9592
krasotun <servrostov@gmail.com>
2023-01-20 21:09:15 +0300

    add:spinner button test

--- packages/client/src/components/SpinnerButton/SpinnerButton.test.tsx
+++ packages/client/src/components/SpinnerButton/SpinnerButton.test.tsx
@@ -5,14 +5,27 @@ describe('#SpinnerButton', () => {
   it('should create', () => {
     const renderedSpinnerButton = render(<SpinnerButton loading />)
     expect(renderedSpinnerButton).toBeTruthy()
-    renderedSpinnerButton.unmount()
   })
-  it('should render with correct text', async () => {
+  it('should render with default text if text not provided in props', () => {
     render(<SpinnerButton loading />)
     expect(screen.getByRole('button')).toHaveTextContent('Секундочку')
   })
-  it('should render with correct text', async () => {
+  it('should render with text from props', () => {
+    render(<SpinnerButton loading spinnerText="Минуточку" />)
+    expect(screen.getByRole('button')).toHaveTextContent('Минуточку')
+  })
+  it('should be disabled if loading true', () => {
     render(<SpinnerButton loading />)
-    expect(screen.getByRole('button')).toHaveTextContent('Пердулечку')
+    expect(screen.getByRole('button')).toHaveAttribute('disabled')
+  })
+  it('spinner should  be rendered if loading true', () => {
+    const { container } = render(<SpinnerButton loading />)
+    const span = container.querySelector('span')
+    expect(span).toBeTruthy()
+  })
+  it('spinner should not be rendered if loading false', () => {
+    const { container } = render(<SpinnerButton loading={false} />)
+    const span = container.querySelector('span')
+    expect(span).toBe(null)
   })
 })
