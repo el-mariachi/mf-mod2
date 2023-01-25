2969a0510c70262391e820e71323ccc8395b2f44
krasotun <servrostov@gmail.com>
2023-01-25 14:02:25 +0300

    fix:all test witt library functions

--- packages/client/src/components/FormControl/FormControl.test.tsx
+++ packages/client/src/components/FormControl/FormControl.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import FormControl from './index'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 describe('#FormControl', () => {
   const registerCallbackStub = () => {}
@@ -36,46 +36,10 @@ describe('#FormControl', () => {
     )
     expect(renderedFormControl).toBeTruthy()
   })
-  it('prop formName should be applied to label and input', () => {
-    const { container } = render(
+  it('prop label should be applied as label text', () => {
+    render(
       <FormControl
         formName={'testName'}
-        controlProps={{
-          name: 'name',
-          label: 'label',
-          type: 'type',
-        }}
-        register={registerCallbackStub as any}
-        errors={[123] as any}
-      />
-    )
-    const label = container.querySelector('label')
-    expect(label?.htmlFor).toBe('testName-name')
-    const input = container.querySelector('input')
-    expect(input?.id).toBe('testName-name')
-  })
-  it('prop name should be applied to label and input as -suffix', () => {
-    const { container } = render(
-      <FormControl
-        formName={'formName'}
-        controlProps={{
-          name: 'testName',
-          label: 'label',
-          type: 'type',
-        }}
-        register={registerCallbackStub as any}
-        errors={[123] as any}
-      />
-    )
-    const label = container.querySelector('label')
-    expect(label?.htmlFor).toBe('formName-testName')
-    const input = container.querySelector('input')
-    expect(input?.id).toBe('formName-testName')
-  })
-  it('prop label should be applied to label as text', () => {
-    const { container } = render(
-      <FormControl
-        formName={'formName'}
         controlProps={{
           name: 'name',
           label: 'testLabel',
@@ -85,27 +49,11 @@ describe('#FormControl', () => {
         errors={[123] as any}
       />
     )
-    const label = container.querySelector('label')
-    expect(label?.textContent).toBe('testLabel')
+    expect(screen.getByLabelText('testLabel')).toBeInTheDocument()
   })
-  it('prop type should be applied to input as type', () => {
-    const { container } = render(
-      <FormControl
-        formName={'formName'}
-        controlProps={{
-          name: 'name',
-          label: 'label',
-          type: 'checkbox',
-        }}
-        register={registerCallbackStub as any}
-        errors={[123] as any}
-      />
-    )
-    const input = container.querySelector('input')
-    expect(input?.type).toBe('checkbox')
-  })
+
   it('prop placeholder should be applied to input as placeholder', () => {
-    const { container } = render(
+    render(
       <FormControl
         formName={'formName'}
         controlProps={{
@@ -118,24 +66,6 @@ describe('#FormControl', () => {
         errors={[123] as any}
       />
     )
-    const input = container.querySelector('input')
-    expect(input?.placeholder).toBe('testPlaceholder')
-  })
-  it('input should have readonly attibute if readOnly prop true', () => {
-    const { container } = render(
-      <FormControl
-        formName={'formName'}
-        controlProps={{
-          name: 'name',
-          label: 'label',
-          type: 'type',
-        }}
-        register={registerCallbackStub as any}
-        errors={[123] as any}
-        readOnly
-      />
-    )
-    const input = container.querySelector('input')
-    expect(input).toHaveAttribute('readonly')
+    expect(screen.getByPlaceholderText('testPlaceholder')).toBeInTheDocument()
   })
 })
