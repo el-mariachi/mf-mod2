2a2472e9d945c8db66f17a7342f07381b9fc64b7
Dmitri Sokolov <mitson.mbox@gmail.com>
2023-01-30 19:48:44 +0300

    test: Tune-up Jest config for css & alias imports

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -8,5 +8,18 @@ export default {
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  moduleNameMapper: {
+    '\\.s?css$': '<rootDir>/src/test/cssStub',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@api/(.*)': '<rootDir>/src/api/$1',
+    '@pages/(.*)': '<rootDir>/src/pages/$1',
+    '@services/(.*)': '<rootDir>/src/services/$1',
+    '@game/(.*)': '<rootDir>/src/game/$1',
+    '@scenes/(.*)': '<rootDir>/src/game/scenes/$1',
+    '@hooks/(.*)': '<rootDir>/src/hooks/$1',
+    '@store/(.*)': '<rootDir>/src/store/$1',
+    '@constants/(.*)': '<rootDir>/src/constants/$1',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
 }
