ce54bdabba75367c77c5382898cee2bebb7aea4e
FrankyHErus <110826856+FrankyHErus@users.noreply.github.com>
2023-02-06 13:03:05 +0300

    ref: Changed names of css classes

---
+++ Modal.tsx
@@ -0,0 +1,28 @@
+import './Modal.scss'
+import ModalProps from './ModalProps'
+
+const Modal = ({ active, children, close, title }: ModalProps) => {
+  return (
+    <div
+      className={active ? 'gameModalUI gameModalUI_active' : 'gameModalUI'}
+      onClick={() => {
+        close()
+      }}>
+      <div className="gameModalUI__content" onClick={e => e.stopPropagation()}>
+        <div className="gameModalUI__header">
+          {title}
+          <button
+            className="modal__close-btn"
+            onClick={() => {
+              close()
+            }}>
+            X
+          </button>
+        </div>
+        <div className="gameModalUI__child-content">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
