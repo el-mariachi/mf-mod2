9c6473265b8c8399cbdb9360b5618ff9e450b8a7
Dmitri Sokolov <mitson.mbox@gmail.com>
2023-03-04 14:54:43 +0300

    fix: Remove store duplicate instance

--- packages/client/src/store/slices/game.ts
+++ packages/client/src/store/slices/game.ts
@@ -24,108 +24,103 @@ const updateTotals = (state: GameSlice) => {
   state.score += computeScore(state.levelStats, state.currentLevel)
 }
 
-const slicer = (initState: GameSlice) =>
-  createSlice({
-    name: 'game',
-    initialState: initState,
-    reducers: {
-      // levels
-      startLevel(state, action: PayloadAction<number>) {
-        const nextLevel = action.payload
-        if (nextLevel > state.totalLevels) {
-          // просто защита. Решение начинать/не начинать уровень по идее принимает контроллер
-          return
-        }
-        return {
-          ...state,
-          currentLevel: nextLevel,
-          levelStats: initState.levelStats,
-          levelComplete: false,
-          currentScene: SCENES.MAP_SCENE,
-          lifeControllerState: LifeControllerState.RUNNING,
-        }
-      },
-      endLevel(state) {
-        state.levelComplete = true
-        state.lifeControllerState = LifeControllerState.PAUSED
-        updateTotals(state)
-        state.currentScene = SCENES.RESULT_SCENE
-      },
-      // game
-      pauseGame(state) {
-        state.lifeControllerState = LifeControllerState.PAUSED
-      },
-      resumeGame(state) {
-        state.currentScene = SCENES.MAP_SCENE
-        state.lifeControllerState = LifeControllerState.RUNNING
-      },
-      exitGame(state) {
-        state.currentScene = SCENES.LOAD_SCENE
-        state.lifeControllerState = LifeControllerState.PAUSED
-        state.levelStats = initState.levelStats
-      },
-      die(state) {
-        // DEPRICATED
-        state.lifeControllerState = LifeControllerState.PAUSED
-        state.currentScene = SCENES.RESULT_SCENE
-      },
-      regInteraction(state, action: PayloadAction<GameSlice['interaction']>) {
-        state.interaction = action.payload
-      },
-      clearInteractions(state) {
-        state.interaction = noInteraction
-      },
-      // scenes
-      showLoadScene(state) {
-        // TODO скорее всего, не нужно здесь
-        state.currentScene = SCENES.LOAD_SCENE
-      },
-      showStartScene(state) {
-        // Используется в хуке useNavToGame. Возможно будет удаляться
-        state.currentScene = SCENES.START_SCENE
-      },
-      showResultScene(state) {
-        state.currentScene = SCENES.RESULT_SCENE
-      },
-      // stats
-      resetTotals(state) {
-        state.gameTotals = initState.gameTotals
-        state.score = initState.score
-      },
-      cancelLevelStats(state, action: PayloadAction<GameSlice['levelStats']>) {
-        const statDeltas = action.payload
-        state.gameTotals = {
-          ...state.gameTotals,
-          ...Object.keys(statDeltas).reduce((result, current) => {
-            const key = current as keyof typeof statDeltas
-            return Object.assign(result, {
-              [current]: state.gameTotals[key] - (statDeltas[key] || 0),
-            })
-          }, {}),
-        }
-        state.score -= computeScore(statDeltas, state.currentLevel)
-      },
-      updateStats(
-        state,
-        action: PayloadAction<Partial<GameSlice['levelStats']>>
-      ) {
-        const statDeltas = action.payload
-        state.levelStats = {
-          ...state.levelStats,
-          ...Object.keys(statDeltas).reduce((result, current) => {
-            const key = current as keyof typeof statDeltas
-            return Object.assign(result, {
-              [current]: state.levelStats[key] + (statDeltas[key] || 0),
-            })
-          }, {}),
-        }
-      },
+const gameSlice = createSlice({
+  name: 'game',
+  initialState: gameInitialState,
+  reducers: {
+    // levels
+    startLevel(state, action: PayloadAction<number>) {
+      const nextLevel = action.payload
+      if (nextLevel > state.totalLevels) {
+        // просто защита. Решение начинать/не начинать уровень по идее принимает контроллер
+        return
+      }
+      return {
+        ...state,
+        currentLevel: nextLevel,
+        levelStats: gameInitialState.levelStats,
+        levelComplete: false,
+        currentScene: SCENES.MAP_SCENE,
+        lifeControllerState: LifeControllerState.RUNNING,
+      }
     },
-  })
-
-const generateSlice = (initState: GameSlice) => slicer(initState).reducer
-
-const gameSlice = slicer(gameInitialState)
+    endLevel(state) {
+      state.levelComplete = true
+      state.lifeControllerState = LifeControllerState.PAUSED
+      updateTotals(state)
+      state.currentScene = SCENES.RESULT_SCENE
+    },
+    // game
+    pauseGame(state) {
+      state.lifeControllerState = LifeControllerState.PAUSED
+    },
+    resumeGame(state) {
+      state.currentScene = SCENES.MAP_SCENE
+      state.lifeControllerState = LifeControllerState.RUNNING
+    },
+    exitGame(state) {
+      state.currentScene = SCENES.LOAD_SCENE
+      state.lifeControllerState = LifeControllerState.PAUSED
+      state.levelStats = gameInitialState.levelStats
+    },
+    die(state) {
+      // DEPRICATED
+      state.lifeControllerState = LifeControllerState.PAUSED
+      state.currentScene = SCENES.RESULT_SCENE
+    },
+    regInteraction(state, action: PayloadAction<GameSlice['interaction']>) {
+      state.interaction = action.payload
+    },
+    clearInteractions(state) {
+      state.interaction = noInteraction
+    },
+    // scenes
+    showLoadScene(state) {
+      // TODO скорее всего, не нужно здесь
+      state.currentScene = SCENES.LOAD_SCENE
+    },
+    showStartScene(state) {
+      // Используется в хуке useNavToGame. Возможно будет удаляться
+      state.currentScene = SCENES.START_SCENE
+    },
+    showResultScene(state) {
+      state.currentScene = SCENES.RESULT_SCENE
+    },
+    // stats
+    resetTotals(state) {
+      state.gameTotals = gameInitialState.gameTotals
+      state.score = gameInitialState.score
+    },
+    cancelLevelStats(state, action: PayloadAction<GameSlice['levelStats']>) {
+      const statDeltas = action.payload
+      state.gameTotals = {
+        ...state.gameTotals,
+        ...Object.keys(statDeltas).reduce((result, current) => {
+          const key = current as keyof typeof statDeltas
+          return Object.assign(result, {
+            [current]: state.gameTotals[key] - (statDeltas[key] || 0),
+          })
+        }, {}),
+      }
+      state.score -= computeScore(statDeltas, state.currentLevel)
+    },
+    updateStats(
+      state,
+      action: PayloadAction<Partial<GameSlice['levelStats']>>
+    ) {
+      const statDeltas = action.payload
+      state.levelStats = {
+        ...state.levelStats,
+        ...Object.keys(statDeltas).reduce((result, current) => {
+          const key = current as keyof typeof statDeltas
+          return Object.assign(result, {
+            [current]: state.levelStats[key] + (statDeltas[key] || 0),
+          })
+        }, {}),
+      }
+    },
+  },
+})
 
 export const {
   startLevel,
@@ -177,7 +172,7 @@ export const nextLevel =
     dispatch(startLevel(currentLevel + 1))
   }
 
-export default generateSlice
+export default gameSlice.reducer
 
 export { LifeControllerState, GameIntaractions }
 export type { GameStats, GameIntaractionDef }
--- packages/client/src/store/slices/hero.ts
+++ packages/client/src/store/slices/hero.ts
@@ -5,84 +5,81 @@ import {
   AnyAction,
 } from '@reduxjs/toolkit'
 import type { RootState } from '@store/index'
-import { die } from '@store/slices/game'
+// import { die } from '@store/slices/game'
 import { createRangeKeeper } from '@utils/index'
 import { heroPresets, heroInitialState } from '@constants/hero'
 import type { HeroSlice } from '@constants/hero'
 
-const slicer = (initState: HeroSlice) =>
-  createSlice({
-    name: 'hero',
-    initialState: initState,
-    reducers: {
-      setHeroClass(state, action: PayloadAction<HeroSlice['heroClass']>) {
-        state.heroClass = action.payload
-        state.health = initState.maxHealth
-        state.resources = heroPresets[state.heroClass]
-        state.resourceMaxValues = initState.resourceMaxValues
-      },
-      resetHeroResources(state) {
-        state.resources = heroPresets[state.heroClass]
-        state.health = initState.maxHealth
-      },
-      updateHealth(state, action: PayloadAction<number>): void {
-        const newValue = state.health + action.payload
-        const keepHealthInRange = createRangeKeeper(0, state.maxHealth)
-        state.health = keepHealthInRange(newValue)
-      },
-      setHealthMaxValue(state, action: PayloadAction<number>) {
-        state.maxHealth = action.payload
-      },
-      setResource(
-        // just setst the resource(s) to specified value
-        // allows any subset of resources
-        // no limit checks
-        state,
-        action: PayloadAction<Partial<(typeof initState)['resources']>>
-      ) {
-        state.resources = {
-          ...state.resources,
-          ...action.payload,
-        }
-      },
-      updateResourceByAmount(
-        // will update the provided resource(s) with delta(s)
-        // limits resulting value to range
-        state,
-        action: PayloadAction<Partial<(typeof initState)['resources']>>
-      ) {
-        const resourceDeltas = action.payload
-        state.resources = {
-          ...state.resources,
-          ...Object.keys(resourceDeltas).reduce((result, current) => {
-            const key = current as keyof typeof resourceDeltas
-            const keepResourceInRange = createRangeKeeper(
-              0,
-              state.resourceMaxValues[key]
-            )
-            return Object.assign(result, {
-              [current]: keepResourceInRange(
-                state.resources[key] + (resourceDeltas[key] || 0)
-              ),
-            })
-          }, {}),
-        }
-      },
-      setResourceMaxValue(
-        state,
-        action: PayloadAction<Partial<(typeof initState)['resourceMaxValues']>>
-      ) {
-        state.resourceMaxValues = {
-          ...state.resourceMaxValues,
-          ...action.payload,
-        }
-      },
+const heroSlice = createSlice({
+  name: 'hero',
+  initialState: heroInitialState,
+  reducers: {
+    setHeroClass(state, action: PayloadAction<HeroSlice['heroClass']>) {
+      state.heroClass = action.payload
+      state.health = heroInitialState.maxHealth
+      state.resources = heroPresets[state.heroClass]
+      state.resourceMaxValues = heroInitialState.resourceMaxValues
     },
-  })
-
-const generateSlice = (initState: HeroSlice) => slicer(initState).reducer
-
-const heroSlice = slicer(heroInitialState)
+    resetHeroResources(state) {
+      state.resources = heroPresets[state.heroClass]
+      state.health = heroInitialState.maxHealth
+    },
+    updateHealth(state, action: PayloadAction<number>): void {
+      const newValue = state.health + action.payload
+      const keepHealthInRange = createRangeKeeper(0, state.maxHealth)
+      state.health = keepHealthInRange(newValue)
+    },
+    setHealthMaxValue(state, action: PayloadAction<number>) {
+      state.maxHealth = action.payload
+    },
+    setResource(
+      // just setst the resource(s) to specified value
+      // allows any subset of resources
+      // no limit checks
+      state,
+      action: PayloadAction<Partial<(typeof heroInitialState)['resources']>>
+    ) {
+      state.resources = {
+        ...state.resources,
+        ...action.payload,
+      }
+    },
+    updateResourceByAmount(
+      // will update the provided resource(s) with delta(s)
+      // limits resulting value to range
+      state,
+      action: PayloadAction<Partial<(typeof heroInitialState)['resources']>>
+    ) {
+      const resourceDeltas = action.payload
+      state.resources = {
+        ...state.resources,
+        ...Object.keys(resourceDeltas).reduce((result, current) => {
+          const key = current as keyof typeof resourceDeltas
+          const keepResourceInRange = createRangeKeeper(
+            0,
+            state.resourceMaxValues[key]
+          )
+          return Object.assign(result, {
+            [current]: keepResourceInRange(
+              state.resources[key] + (resourceDeltas[key] || 0)
+            ),
+          })
+        }, {}),
+      }
+    },
+    setResourceMaxValue(
+      state,
+      action: PayloadAction<
+        Partial<(typeof heroInitialState)['resourceMaxValues']>
+      >
+    ) {
+      state.resourceMaxValues = {
+        ...state.resourceMaxValues,
+        ...action.payload,
+      }
+    },
+  },
+})
 
 export const {
   resetHeroResources,
@@ -96,7 +93,7 @@ export const {
 
 export const updateHealthByAmount =
   (amount: number): ThunkAction<void, RootState, unknown, AnyAction> =>
-  (dispatch, getState) => {
+  dispatch => {
     dispatch(updateHealth(amount))
     // it could be handy, but buisness-logic in store makes controller code tricky oO
     // const { hero } = getState()
@@ -105,4 +102,4 @@ export const updateHealthByAmount =
     // }
   }
 
-export default generateSlice
+export default heroSlice.reducer
