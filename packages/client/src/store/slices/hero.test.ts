804d8c8fc2875066e80daea6fe706583ee83c83f
Dmitri Sokolov <mitson.mbox@gmail.com>
2023-03-04 16:38:15 +0300

    test: Update hero slice test

--- packages/client/src/store/slices/hero.test.ts
+++ packages/client/src/store/slices/hero.test.ts
@@ -14,16 +14,12 @@ const defaultHero = {
   resourceMaxValues: defaultMaxValues,
 }
 
-const heroReducer = reducer(defaultHero)
-
 describe('Testing player slice', () => {
   it('should return the initial state', () => {
-    expect(heroReducer(undefined, { type: undefined })).toEqual(defaultHero)
+    expect(reducer(undefined, { type: undefined })).toEqual(defaultHero)
   })
   it('should update the skills with provided payload', () => {
-    expect(
-      heroReducer(defaultHero, updateResourceByAmount({ hits: 64 }))
-    ).toEqual({
+    expect(reducer(defaultHero, updateResourceByAmount({ hits: 64 }))).toEqual({
       ...defaultHero,
       resources: {
         hits: 96,
@@ -31,23 +27,23 @@ describe('Testing player slice', () => {
     })
   })
   it('should not set the skill above max value', () => {
-    expect(
-      heroReducer(defaultHero, updateResourceByAmount({ hits: 640 }))
-    ).toEqual({
-      ...defaultHero,
-      resources: {
-        hits: defaultMaxValues.hits,
-      },
-    })
+    expect(reducer(defaultHero, updateResourceByAmount({ hits: 640 }))).toEqual(
+      {
+        ...defaultHero,
+        resources: {
+          hits: defaultMaxValues.hits,
+        },
+      }
+    )
   })
   it('should update health with provided payload', () => {
-    expect(heroReducer(defaultHero, updateHealth(-42))).toEqual({
+    expect(reducer(defaultHero, updateHealth(-42))).toEqual({
       ...defaultHero,
       health: 58,
     })
   })
   it('should not set health above max value', () => {
-    expect(heroReducer(defaultHero, updateHealth(120))).toEqual({
+    expect(reducer(defaultHero, updateHealth(120))).toEqual({
       ...defaultHero,
       health: 100,
     })
