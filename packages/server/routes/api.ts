bb4db4abc4c092ef570fe6b4cfb53a7d37a9547e
gineff <canone@inbox.ru>
2023-03-08 11:50:09 +0300

    fx: fix api router

--- packages/server/routes/api.ts
+++ packages/server/routes/api.ts
@@ -4,7 +4,8 @@ import { proxy } from '../middleware/proxy'
 
 const apiRouter = Router()
 
-apiRouter.use('/yandex', proxy)
 apiRouter.use('/topic', TopicApi)
+/** все остальные запросы будут проксированы на ya-praktikum.tech  */
+apiRouter.use('/', proxy)
 
 export default apiRouter
