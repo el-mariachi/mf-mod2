39299452d99cfa8f7f884a3e714e535e59f13c2a
gineff <canone@inbox.ru>
2023-03-08 11:47:15 +0300

    ft: add Comment api

--- packages/server/api/Topic.ts
+++ packages/server/api/Topic.ts
@@ -1,44 +1,49 @@
 import { Topic } from '../db/models/Topic'
+import { User } from '../db/models/User'
 import { type Request, type Response, Router } from 'express'
 import { checkAuthMiddleware } from '../middleware/checkAuth'
 
 export const TopicApi = Router()
 
 TopicApi.get('/:id', (req: Request, res: Response) => {
-  Topic.findByPk(req.params.id).then(topic =>
+  Topic.findByPk(req.params.id, { include: [{ model: User }] }).then(topic =>
     topic
       ? res.status(200).json(topic)
-      : res.status(400).json({ type: 'error', message: 'Topic not found' })
+      : res.status(404).json({ type: 'error', message: 'Topic not found' })
   )
 })
 
-TopicApi.get('/', (_: Request, res: Response) => {
-  console.log('topic')
-  Topic.findAll().then(topic => res.status(200).json(topic))
+TopicApi.get('/', checkAuthMiddleware, (_: Request, res: Response) => {
+  Topic.findAll({ include: [{ model: User }] })
+    .then(topic => res.status(200).json(topic))
+    .catch(err => res.status(500).end(err.message))
 })
 
-TopicApi.post('/', (req: Request, res: Response) => {
+TopicApi.post('/', checkAuthMiddleware, (req: Request, res: Response) => {
   req.body.user_id = res.locals.user.id
-  Topic.create(req.body).then(topic => res.status(201).send({ id: topic.id }))
+  Topic.create(req.body)
+    .then(topic => res.status(201).send({ id: topic.id }))
+    .catch(err => res.status(500).end(err.message))
 })
 
-
-TopicApi.put('/:id', (req: Request, res: Response) => {
+TopicApi.put('/:id', checkAuthMiddleware, (req: Request, res: Response) => {
   Topic.update(req.body, {
-    where: { id: req.params.id, user_id: res.locals.user.id },
+    where: { id: req.params.id },
     returning: true,
   })
     .then(result => {
       const [count, topics] = result
       if (count === 0) {
-        throw Error('Топик не найден')
+        res.status(404).json({ type: 'error', message: 'Topic not found' })
+      } else {
+        res.status(200).json(topics[0])
       }
-      res.status(200).json(topics[0])
     })
+    .catch(err => res.status(500).end(err.message))
 })
 
-TopicApi.delete('/:id', (req: Request, res: Response, next) => {
-    Topic.destroy({ where: { id: req.params.id, user_id: res.locals.user.id } })
-      .then(topic => res.status(201).json(topic))
-      .catch(next)
+TopicApi.delete('/:id', checkAuthMiddleware, (req: Request, res: Response) => {
+  Topic.destroy({ where: { id: req.params.id } })
+    .then(topic => res.status(201).json(topic))
+    .catch(err => res.status(500).end(err.message))
 })
