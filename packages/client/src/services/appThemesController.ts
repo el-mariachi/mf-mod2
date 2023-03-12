43af040f43506174cff45406d0beb67588552f05
alexey-ryabkov <xelaris@ya.ru>
2023-03-12 14:27:13 +0300

    ref: clear off debug

--- packages/client/src/services/appThemesController.ts
+++ packages/client/src/services/appThemesController.ts
@@ -7,13 +7,10 @@ import { apiErrorHandler } from '@utils/errorsHandling'
 export default new (class {
   protected _themes: Record<string, number> = {}
   getList() {
-    // return Promise.resolve(['default', 'acid', 'doomer', 'frozen', 'mint'])
-    console.log('getList')
     return appThemesApi.getAppThemes()
       .then(themesData => {
         this._themes = {}
         return themesData.map(themeData => {
-          console.log(themeData);
           const {id, theme} = themeData
           this._themes[theme] = id
           return theme
@@ -21,19 +18,15 @@ export default new (class {
       })
       .catch(error => apiErrorHandler(error))
   }
-  getTheme() {
-    // return Promise.resolve('default')
-    console.log('getTheme')
+  getTheme() {  
     if (this._userId) {
       return appUsersApi.getAppUser(this._userId)
-        .then(userData => {console.log(userData); return userData.user_theme?.theme.theme || ''}) 
+        .then(userData => userData.user_theme?.theme.theme || '') 
         .catch(error => apiErrorHandler(error))
     }
     return Promise.resolve('')
   }
   setTheme(themeName: string) {
-    // return Promise.resolve()
-    console.log('setTheme')
     if (this._userId && themeName in this._themes) {
       const themeId = this._themes[themeName]
       return appUsersApi.setAppUserTheme(this._userId, themeId)
