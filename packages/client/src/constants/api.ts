c997359cd0a9ca486218ab7c6007b12319225208
Dmitri Sokolov <mitson.mbox@gmail.com>
2023-03-23 12:45:25 +0300

    fix: OAuth redirect URL

--- packages/client/src/constants/api.ts
+++ packages/client/src/constants/api.ts
@@ -3,7 +3,7 @@ const API_TIMEOUT = 5000
 /** Для редиректа зарегистрирован адрес localhost:3000
  * Поэтому OAuth можно использовать только запустив проект в контейнерах client, server, postgres
  */
-const API_OAUTH_REDIRECT_URL = `http://team7-onebit-dungeon.ya-praktikum.tech/`
+const API_OAUTH_REDIRECT_URL = `https://team7-onebit-dungeon.ya-praktikum.tech`
 const API_YANDEX_OAUTH_URL = `https://oauth.yandex.ru/authorize?response_type=code`
 export const TEAM_NAME_LB_API = 'team-seven'
 
